90b037cdc2755ce6f8b930037241f317f661eb4b
Golf0ned <benjaminye.email@gmail.com>
2024-03-28 14:28:43 -0500

    hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\benja\Dropbox\Projects\github\rustbook\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\benja\Dropbox\Projects\github\rustbook\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\benja\Dropbox\Projects\github\rustbook\hello_cargo\target\debug\hello_cargo.exe: C:\Users\benja\Dropbox\Projects\github\rustbook\hello_cargo\src\main.rs
